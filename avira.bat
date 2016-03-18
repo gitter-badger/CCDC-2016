b7370cb7023f78a99628e12aacfc2f23fcbc44f8
Jebbraughler <jebbraughler@gmail.com>
2016-03-18 19:40:03 -0400

    Downloads the avira installation exe

---
+++ avira.bat
@@ -0,0 +1,2 @@
+powershell -Command "(New-Object Net.WebClient).DownloadFile('http://www.avira.com/downloads/frontendDl/defaultDownload?product=72', 'avira.exe')"
+powershell -Command "Invoke-WebRequest http://www.avira.com/downloads/frontendDl/defaultDownload?product=72 -OutFile avira.exe"
